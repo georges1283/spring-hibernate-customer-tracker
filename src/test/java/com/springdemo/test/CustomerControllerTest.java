f73a5f2bb87dfce96308973b78b0e405c567adc5
georges1283 <47242653+georges1283@users.noreply.github.com>
2022-08-10 20:28:32 -0400

    Commit 3

--- src/test/java/com/springdemo/test/CustomerControllerTest.java
+++ src/test/java/com/springdemo/test/CustomerControllerTest.java
@@ -1,6 +1,7 @@
 package com.springdemo.test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
@@ -14,8 +15,12 @@ import org.junit.platform.runner.JUnitPlatform;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.internal.stubbing.answers.Returns;
+import org.mockito.internal.stubbing.defaultanswers.ReturnsSmartNulls;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
@@ -23,19 +28,21 @@ import com.springdemo.controller.CustomerController;
 import com.springdemo.entity.Customer;
 import com.springdemo.service.CustomerServiceImpl;
 
-
 @ExtendWith(MockitoExtension.class)
 @RunWith(JUnitPlatform.class)
 public class CustomerControllerTest {
-	
+
 	@InjectMocks
 	CustomerController customerController;
-	
+
 	@Mock
 	CustomerServiceImpl customerService;
 
 	@Mock
 	Customer customerDao;
+
+	@Mock
+	Model theModel;
 
 	@Test
 	public void testFindAll() {
@@ -43,37 +50,58 @@ public class CustomerControllerTest {
 		Customer customer1 = new Customer(1, "Test", "Tester", "Testing@gmail.com");
 		Customer customer2 = new Customer(2, "Test", "Gussin", "example@gmail.com");
 		List<Customer> customers = Arrays.asList(customer1, customer2);
+		theModel.addAttribute("customers", customer1);
+		theModel.addAttribute("customers", customer2);
 
-		int sort = 0;
-		when(customerService.getCustomers(sort)).thenReturn(customers);
-		 
-        // when
-		List<Customer> result = customerService.getCustomers(sort);
+		String sort = "0";
+		// when
+		String responseEntity = customerController.listCustomers(theModel, sort);
+
+		assertEquals(responseEntity, "list-customers");
+
+	}
+
+	@Test
+	public void testShowFormForAdd() {
+		// Customer customer = new Customer("Tester", "Test", "testingggg@gmail.com");
+		String responseEntity = customerController.showFormForAdd(theModel);
+
+		assertEquals(responseEntity, "customer-form");
+	}
+
+	@Test
+	public void testSaveCustomer() {
+		Customer customer = new Customer("Tester", "Test", "testingggg@gmail.com");
 		
-//		when(customerService.getCustomers(eq(sort))).thenReturn(Arrays.asList(customer1, customer2));
+		String responseEntity = customerController.saveCustomer(customer);
 		
-		// then
-        assertThat(result.size()).isEqualTo(2);
-         
-        assertThat(result.get(0).getFirstName())
-                        .isEqualTo(customer1.getFirstName());
-         
-        assertThat(result.get(1).getFirstName())
-                        .isEqualTo(customer2.getFirstName());
-		
+		assertEquals(responseEntity, "redirect:/customer/list");
+	}
+
+	@Test
+	public void testShowFormForUpdate() {
+		int customerId = 1;
+		String responseEntity = customerController.showFormForUpdate(customerId, theModel);
+
+		assertEquals(responseEntity, "customer-form");
 	}
 	
 	@Test
-	public void testAddCustomer() {
-		MockHttpServletRequest request = new MockHttpServletRequest();
-        RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
-         
-        when(customerController.saveCustomer(any(Customer.class))).thenReturn("redirect:/customer/list");
-         
-        Customer customer = new Customer("Tester", "Test", "testingggg@gmail.com");
-        String responseEntity = customerController.saveCustomer(customer);
-         
-        assertThat(responseEntity = "redirect:/customer/list");
+	public void TestDeleteCustomer() {
+		int customerId = 1;
+		
+		String responseEntity =  customerController.deleteCustomer(customerId);
+		
+		assertEquals(responseEntity, "redirect:/customer/list");
+	}
+	
+	@Test
+	public void TestSearchCustomer() {
+		String customerSearchName = "Test";
+		
+		String responseEntity =  customerController.searchCustomer(customerSearchName, theModel);
+		
+		assertEquals(responseEntity, "list-customers");
 	}
 
 }
